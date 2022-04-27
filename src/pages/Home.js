5bef7e6b5d882c33a20c52303b29685fa2ea2d12
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-04-27 18:15:57 +0200

    added data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import backgroundImg from "../background.png";
-
+import data from "../data.json";
 const url =
   "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json";
 
 const Home = () => {
+  console.log(data);
+
   return (
     <>
       <section className="homeHeader">
