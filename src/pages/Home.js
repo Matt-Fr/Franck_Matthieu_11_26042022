7f7fb4bec15d748d48ad13196611e2bda5450390
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-04-27 11:16:10 +0200

    added background home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import backgroundImg from "../background.png";
+
+const url =
+  "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="homeHeader">
+        <img src={backgroundImg} alt="" className="homeHeader-img" />
+        <h1 className="homeHeader-title">Chez vous, partout et ailleurs</h1>
+      </section>
+      <section className="thumbContainer"></section>
+    </>
+  );
 };
 
 export default Home;
