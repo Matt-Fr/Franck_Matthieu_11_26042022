2e5bdaeccc7c58ab2f1c79013ce9cd85c6dd3345
Matthieu FRANCK <matthieu_franck@orange.fr>
2022-05-03 13:30:57 +0200

    finished slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-const slider = ({ pictures }) => {
-  console.log(pictures);
+const Slider = ({ pictures }) => {
+  const [images, setImages] = useState(pictures);
+  const [index, setIndex] = useState(0);
+  console.log(index);
+
+  useEffect(() => {
+    const lastIndex = images.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, images]);
+
   return (
     <div className="imgContainer">
-      {pictures.map((picture) => {
-        return (
-          <img src={picture} alt="" className="imgContainer-activeSlide" />
-        );
+      {images.map((picture, picindex) => {
+        let position = "imgContainer-nextSlide";
+        if (picindex === index) {
+          position = "imgContainer-activeSlide";
+        }
+        if (
+          picindex === index - 1 ||
+          (index === 0 && picindex === images.length - 1)
+        ) {
+          position = "imgContainer-prevSlide";
+        }
+        return <img src={picture} alt="" className={position} />;
       })}
-      <button className="prev">
+      <button
+        className="prev"
+        onClick={() => {
+          setIndex(index - 1);
+        }}
+      >
         <FiChevronLeft />
       </button>
-      <button className="next">
+      <button
+        className="next"
+        onClick={() => {
+          setIndex(index + 1);
+        }}
+      >
         <FiChevronRight />
       </button>
+      <span className="indexImg">{`${index + 1} / ${images.length}`}</span>
     </div>
   );
 };
 
-export default slider;
+export default Slider;
